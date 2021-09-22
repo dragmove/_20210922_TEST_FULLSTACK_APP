bea5830b3de1827f1cda8d465c00dda8d0df38f8
hyunseok.kim <hyunseok.kim@42dot.ai>
2021-09-23 00:47:26 +0900

    feat: 1st commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# \_20210222_TEST_DOCKER_FULLSTACK_APP
+
+## About
+
+- ing
+
+### Setting
+
+- ing
+
+## Contact
+
+- @Website : https://dragmove-df96f.web.app/
+- @Blog : http://blog.naver.com/dragmove
+- @LinkedIn : https://www.linkedin.com/in/hyunseok-kim-99748295/
+- @E-mail : dragmove@gmail.com
+
+## License
+
+[MIT license](http://danro.mit-license.org/).
---
+++ backend/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const db = require("./db");
+
+const app = express();
+
+db.pool.query(
+  `CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT,
+    PRIMARY KEY (id)
+  )`,
+  (err, results, fields) => {
+    console.error(err);
+    console.log("results :", results);
+  }
+);
+
+app.use("/api/values", (req, res) => {
+  db.pool.query("SELECT * FROM lists;", (err, results, fields) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+
+    return res.json(results);
+  });
+});
+
+app.post("/api/values", (req, res, next) => {
+  db.pool.query(
+    `INSERT INTO lists (value) VALUES("${req.body.value}")`,
+    (err, results, fields) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send(err);
+      }
+
+      return res.json({
+        success: true,
+        value: req.body.value,
+      });
+    }
+  );
+});
+
+app.listen(5000, () => {
+  console.log("App starts on port 5000");
+});
